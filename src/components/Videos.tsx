5b2423c9d5ae3f3db98c7d890762ace3335f43df
Juan Cedeño <juancc0315@gmail.com>
2021-02-03 04:07:02 +0800

    add loading

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -3,18 +3,21 @@ import { Video } from "../interfaces/video";
 import VideoService from "../services/video";
 import { VideoItems } from "./VideoItems";
 import '../css/video.css'
+import { Spinner } from "./Spinner";
 
 export const Videos = () => {
 
      const [videos, setVideos] = useState<Video[]>([])
+     const [loading, setLoading] = useState<boolean>(false)
 
      useEffect(() => {
+          setLoading(true)
           const getVideos = async () => {
             const data = await VideoService.getVideos()
             setVideos(data)
+            setLoading(false)
             
           }
-          
           getVideos()
        }, [])
 
@@ -22,6 +25,10 @@ export const Videos = () => {
             VideoService.deleteVideo(id)
             const videoDeleted = videos.filter(video => video._id !== id)
             setVideos(videoDeleted)
+       }
+
+       if (loading) {
+            return <Spinner/>
        }
 
      return (
